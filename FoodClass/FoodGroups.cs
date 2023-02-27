03f12084e2d30c1a096da535b98bbb2f5341d433
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2023-02-26 21:05:48 -0600

    Add files via upload

---
+++ FoodClass/FoodGroups.cs
@@ -0,0 +1,14 @@
+﻿namespace FoodClass
+{
+    /// <summary>
+    /// Represents the different food groups.
+    /// </summary>
+    public enum FoodGroups
+    {
+        Grains,
+        Vegetables,
+        Fruit,
+        Protein,
+        Dairy
+    }
+}
---
+++ FoodClass/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Food Class
+ * 2/26/2023
+ * C#.NET I
+ * 
+ * Instructions:
+ * 
+ * Part 1
+ * In this example, we are going to write the Food class, which
+ * will represent a piece of food. Let's start by creating the 
+ * Food class in a seperate file but still in the FoodClass namespace.
+ * 
+ * After creating the Food class, let's add 4 instance variables.
+ * The first will be the name of the food, the second will be the calories
+ * the food contains per serving, the third will be the food group to
+ * which the food belongs (there is an enumaration for the food groups
+ * already provided), and the fourth will be the serving size for the food
+ * in grams.
+ * 
+ * Next, we will write a constructor that initializes all the instance
+ * variables to passed values. Additionally, We will write getter methods 
+ * for each instance variable. We will also write a setter method for
+ * calories and serving size. Both of these setters should only change
+ * the value of the corresponding instance variable if the value is greater
+ * than or equal to 0.
+ * 
+ * Finally, we will write some code in the main to test our class,
+ * 
+ * Part 2
+ * We will rewrite the class to utilize properites instead of getter
+ * and setter methods. Which version of the class do you prefer?
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
